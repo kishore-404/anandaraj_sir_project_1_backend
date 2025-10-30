14f72022701e0527ac1559b30cf450149b0fa495
kishore-404 <kishores.developer@gmail.com>
2025-10-30 15:00:39 +0530

    added mongo url

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,14 @@
-// src/config/db.js
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(`❌ Error: ${err.message}`);
+  } catch (error) {
+    console.error(`❌ Error: ${error.message}`);
     process.exit(1);
   }
 };
